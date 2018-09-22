69815524f63255d6a91d406d9769f80fc0c38c9f
Gery Akbar Fauzi <43455599+geryakbarf@users.noreply.github.com>
2018-09-22 15:47:43 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117049/latihan6/kambingstatic/Mamalia.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117049.latihan6.kambingstatic;
+
+/**
+ *
+ * @author Gery Akbar Fauzi
+ * Nama                 : Gery Akbar Fauzi
+ * Kelas                : PBO2
+ * Nim                  : 10117049
+ * Deskripsi Program    : Program ini berisi program menggabungkan dua class
+ *                        file yang berbeda
+ */
+public class Mamalia {
+    //variabel jumlahKambing dideklarasikan sebagai statik
+    public static int jumlahKambing;
+}
